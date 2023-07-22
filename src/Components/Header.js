fd9ed3fc2f0bdd43e83280179fb366d37f7fe3aa
aakash <marojuaakash2003@gmail.com>
2023-07-22 23:24:53 +0530

    changes made on nav bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,59 +13,59 @@ function Header() {
   };
 
   return (
-    <>
-      <div className='container' style={headerStyles}>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#homeSection">
-            <div className="container-box-fluid">FPLC</div>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#homeSection" onClick={() => scrollToElement('homeSection')}>
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#aboutSection" onClick={() => scrollToElement('aboutSection')}>
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#servicesSection" onClick={() => scrollToElement('servicesSection')}>
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contactSection" onClick={() => scrollToElement('contactSection')}>
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={headerStyles}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#homeSection">
+          FPLC
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#homeSection" onClick={() => scrollToElement('homeSection')}>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#aboutSection" onClick={() => scrollToElement('aboutSection')}>
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#servicesSection" onClick={() => scrollToElement('servicesSection')}>
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contactSection" onClick={() => scrollToElement('contactSection')}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </>
+    </nav>
   );
 }
 
 // CSS styles for the header
 const headerStyles = {
-  position: 'fixed',
-  top: 0,
   width: '100%',
-  height:'5%'
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingLeft: '20px',
+  paddingRight: '20px',
 };
 
 export default Header;
